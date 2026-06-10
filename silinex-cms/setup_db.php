da56b07b81f79831c9367d9e01047e8c8e879f6e
gjayakesh <gjayakesh@gmail.com>
2026-06-10 19:14:23 +0530

    Remove Dockerfile and clean up Docker references

---
+++ silinex-cms/setup_db.php
@@ -0,0 +1,6 @@
+<?php
+// Auto-generated setup script for Silinex CMS database initialization
+// This script simply includes the main DB bootstrap which creates the schema
+// and seeds a default admin user if the database is empty.
+require __DIR__ . '/includes/db.php';
+?>
